4964bb61a5a1fd8a5f00b68c78fe7ffe93a586fe
talharauf-qbatch <talha.rauf@qbatch.com>
2025-08-18 11:41:19 +0500

    Set up initial React project with Vite, including ESLint configuration, Tailwind CSS, and basic file structure. Added .gitignore, package.json, and essential components for development.

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-gray-900 text-white">
+    <h1 className="text-4xl font-bold">Tailwind Works 🚀</h1>
+  </div>
+  )
+}
+
+export default App
